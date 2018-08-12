c17126583523c4bf17861cb3be23929c2101e9f8
Sam <lemonverse@gmail.com>
2018-08-12 22:13:00 +1200

    update update.json again

--- src/main/java/party/lemons/alternatespawner/AlternateSpawn.java
+++ src/main/java/party/lemons/alternatespawner/AlternateSpawn.java
@@ -5,7 +5,7 @@ import net.minecraftforge.fml.common.Mod;
 /**
  * Created by Sam on 12/08/2018.
  */
-@Mod(modid = AlternateSpawn.MODID, version = AlternateSpawn.VERSION, name = AlternateSpawn.NAME)
+@Mod(modid = AlternateSpawn.MODID, version = AlternateSpawn.VERSION, name = AlternateSpawn.NAME, updateJSON = "https://raw.githubusercontent.com/Lemonszz/AlternateSpawn/master/update.json")
 public class AlternateSpawn
 {
 	public static final String MODID = "alternatespawn";
