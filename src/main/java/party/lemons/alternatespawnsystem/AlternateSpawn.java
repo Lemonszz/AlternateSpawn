2da5c3811072c55e16a4cbc14549053473a4b9a1
Sam <lemonverse@gmail.com>
2018-08-15 17:22:59 +1200

    what

--- src/main/java/party/lemons/alternatespawnsystem/AlternateSpawn.java
+++ src/main/java/party/lemons/alternatespawnsystem/AlternateSpawn.java
@@ -4,7 +4,6 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import party.lemons.alternatespawnsystem.proxy.IProxy;
-
 /**
  * Created by Sam on 12/08/2018.
  */
